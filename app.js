ed66bb97a1f7a22cf6f48d1f310cbe1a1c013fc0
Gaurav Prakash Singh Khati <gkhati275@gmail.com>
2024-06-28 13:47:34 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// function  A Reusable block of code  
+    // type according to functionality 
+        // Higher Order function : afunction which takes another function as argument
+        // Callback Function : a function which is passed as argumemnt 
+    // Type according to declaration method
+            // function keyword
+            function name(){
+
+            }
+
+            // arrow fnction
+            let name_functon =()=>{
+                // another way of declaring
+            }
+// functional programming  is using higher order and callback function
+
+        
