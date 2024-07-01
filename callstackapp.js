57805fd13260aebe14de3d3922d9d63abd2bd63e
Gaurav-Prakash-Singh-khati-007 <gkhati275@gmail.com>
2024-07-01 14:16:36 -0700

    call stack code added

--- callstackapp.js
+++ callstackapp.js
@@ -2,12 +2,26 @@
 
 
 
-let a=10;
+// let a=10;
 
-let callstackFunction=()=>{
-    let string ="Money"
-    console.log("You are sweet")
+// let callstackFunction=()=>{
+//     let string =" 1st in function decalred inside function Money"
+//     console.log(string)
+//     console.log(" 2nd in function You are sweet")
+
+// }
+
+// console.log(" declared at 3 and exectued first What you want");
+// callstackFunction()
+
+
+
+let question ="How are you ???"
+
+let call=(question)=>{
+    console.log(question)
+    return "I am fine what about you ??"
 }
 
-console.log("What you want");
-callstackFunction()
+let reply = call(question)
+console.log(reply)
